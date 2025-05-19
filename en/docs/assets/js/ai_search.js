194667b3c44319c422b4a95225df6b99f94698e5
Chilliwiddit <nivedhith@wso2.com>
2025-05-19 14:33:25 +0530

    additional DOM manipulation

--- en/docs/assets/js/ai_search.js
+++ en/docs/assets/js/ai_search.js
@@ -6,27 +6,67 @@ document.addEventListener("DOMContentLoaded", function () {
     const searchStats = document.getElementsByClassName("md-search-result__meta")[0];
     const searchOutput = document.getElementsByClassName("md-search__output")[0];
     const ai_response = document.getElementById("ai_response");
-    const search_container = document.getElementsByClassName("md-search__scrollwrap")[0];
+    // const search_container = document.getElementsByClassName("md-search__scrollwrap")[0];
+    const loader = document.getElementsByClassName("loader")[0];
+
+    let aiResponseText = document.getElementById("ai_response_text");
 
     searchInput.addEventListener("input", function () {
         const searchText = searchInput.value.trim();
 
         if (searchText.length > 0) {
-            aiOption.style.display = "block"; 
+            aiOption.style.display = "flex"; 
             aiQueryText.textContent = `Ask ME about "${searchText}"!`; 
         } else {
             aiOption.style.display = "none"; 
         }
     });
 
-    aiOption.addEventListener("click", function () {
+    aiOption.addEventListener("click", async function () {
+        if (loader.style.display == "none") {
+            loader.style.display = "block";
+        }
         const searchText = searchInput.value.trim();
 
         searchSuggestions.style.display = "none";
         searchStats.style.display = "none";
         ai_response.style.display = "block";
+        aiResponseText.innerHTML = "";
         aiQueryText.textContent = searchText;
-        search_container.style.height = "699px"; 
+
+        const URL = "<URL>"; 
+    
+        const socket = new WebSocket(URL);
+    
+        socket.onopen = () => {
+            console.log("WebSocket connection opened");
+    
+            const payload = {
+                questions: [searchText],
+                question_context: "string",
+                x_request_id: ""
+            };
+            socket.send(JSON.stringify(payload));
+        };
+    
+        let fullContent = "";
+    
+        socket.onmessage = (event) => {
+            const chunk = event.data;
+            fullContent += chunk;
+            aiResponseText.innerHTML = marked.parse(fullContent);
+        };
+    
+        socket.onerror = (error) => {
+            console.error("WebSocket error:", error);
+            aiResponseText.innerHTML = "Error while processing your request. Please try again later.";
+            loader.style.display = "none";
+        };
+    
+        socket.onclose = (event) => {
+            console.log("WebSocket closed:", event.reason);
+            loader.style.display = "none";
+        };
     });
 
     document.addEventListener("click", function (event) {
